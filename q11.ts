81dc0d25a9af7990188f2e9659eac6d7df6a0a04
Sarwat Haroon <159713603+Sarwatharoon68@users.noreply.github.com>
2024-02-25 10:45:28 +0500

    Add files via upload

---
+++ q11.ts
@@ -0,0 +1,11 @@
+/* Ques # 11:
+Names: store the name of a few of your friends in a array called names, print each person's name by accessing each element in the list, one at a time.*/
+
+
+let friends_names : string[] =["Sarwat","Sara","Zoha","Uzma"]
+
+console.log(friends_names);
+console.log(friends_names[0]);
+console.log(friends_names[1]);
+console.log(friends_names[2]);
+console.log(friends_names[3]);
